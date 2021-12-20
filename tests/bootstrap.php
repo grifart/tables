0258e5960023cceab0c169c126f2b4997cc96eef
Jiří Pudil <me@jiripudil.cz>
2021-12-20 13:03:55 +0100

    tests: hint about missing createConnection file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,10 @@ require __DIR__ . '/../vendor/autoload.php';
 Environment::setup();
 
 function connect(): Connection {
+	if ( ! \file_exists(__DIR__ . '/createConnection.local.php')) {
+		throw new \RuntimeException('Missing connection credentials. Please create tests/createConnection.local.php that returns a configured instance of Dibi\Connection.');
+	}
+
 	$connection = require __DIR__ . '/createConnection.local.php';
 	\assert($connection instanceof Connection);
 
