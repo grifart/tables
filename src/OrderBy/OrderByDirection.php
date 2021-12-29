2e906ac04c11af3e979a98554fa67ed21d1b8eee
Jiří Pudil <me@jiripudil.cz>
2021-12-29 15:16:50 +0100

    extract OrderByDirection (to become enum later)

---
+++ src/OrderBy/OrderByDirection.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Grifart\Tables\OrderBy;
+
+final class OrderByDirection
+{
+	public const ASC = 'ASC';
+	public const DESC = 'DESC';
+
+	private function __construct()
+	{
+	}
+}
