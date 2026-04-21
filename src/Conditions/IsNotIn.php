51a0d3582bceb1c7a48d8352ead6813a8a370bad
Jiří Pudil <me@jiripudil.cz>
2026-04-21 14:41:01 +0200

    fix: handle empty array in IsNotIn

--- src/Conditions/IsNotIn.php
+++ src/Conditions/IsNotIn.php
@@ -6,6 +6,7 @@ namespace Grifart\Tables\Conditions;
 
 use Dibi\Expression as DibiExpression;
 use Grifart\Tables\Expression;
+use function count;
 use function Phun\map;
 
 /**
@@ -24,6 +25,11 @@ final class IsNotIn implements Condition
 
 	public function toSql(): DibiExpression
 	{
+		// dibi translates empty array into (NULL) which matches nothing (`NOT IN (NULL)`) but we want to match everything
+		if (count($this->values) === 0) {
+			return new DibiExpression('%b', true);
+		}
+
 		return new DibiExpression(
 			'? NOT IN %in',
 			$this->expression->toSql(),
